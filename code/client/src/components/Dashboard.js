eb14197ec2b9355e9e94874e6752f585da775148
shashkhr25 <shashwatkhare03@gmail.com>
2020-08-16 17:03:08 +0530

    screenshots

--- code/client/src/components/Dashboard.js
+++ code/client/src/components/Dashboard.js
@@ -4,7 +4,11 @@ import { Link } from 'react-router-dom';
 const Dashboard =() =>{
     return(
         <div>
-            Dashboard
+            <div class="center-align">
+                <h1>
+                Welcome to your Home Page!!
+                </h1>
+            </div>
             <div className="fixed-action-btn">
                 <Link to="/surveys/new" className="btn-floating btn-large red">
                     <i className="material-icons">New</i>
